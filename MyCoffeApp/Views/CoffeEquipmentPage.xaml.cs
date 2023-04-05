d75554681a6940ca206b3b5066295376014abc46
MarinCvj <marin@cvjetkovic.me>
2023-04-05 18:03:57 +0200

    Add project files.

---
+++ MyCoffeApp/Views/CoffeEquipmentPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MyCoffeApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CoffeEquipmentPage : ContentPage
+    {
+        public CoffeEquipmentPage()
+        {
+            InitializeComponent();
+        }
+        int counter = 0;
+        private void ButtonClick_Clicked(object sender, EventArgs e)
+        {
+            counter++;
+            LabelCount.Text = $"You have clicked {counter} times!";
+        }
+    }
+}
