72ec83d372b7d492de9437e1d36ce9ac4380d7bd
MarinCvj <marin@cvjetkovic.me>
2023-04-05 18:58:53 +0200

    Adding MVVM structure for optimizing the code

---
+++ MyCoffeApp/ViewModels/CoffeEquipmentViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace MyCoffeApp.ViewModels
+{
+    public class CoffeEquipmentViewModel : BindableObject
+    {
+        public CoffeEquipmentViewModel()
+        {
+            IncreseCount = new Command(OnIncrese);
+        }
+
+        public ICommand IncreseCount { get; }
+
+        int counter = 0;
+        string countDisplay = "Click me!";
+        public string CountDisplay
+        {
+            get => countDisplay;
+            set
+            {
+                if (value == countDisplay)
+                {
+                    return;
+                }
+                else
+                {
+                    countDisplay = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        void OnIncrese()
+        {
+            counter++;
+            CountDisplay = $"You have clicked {counter} times!";
+        }
+    }
+}
--- MyCoffeApp/Views/CoffeEquipmentPage.xaml.cs
+++ MyCoffeApp/Views/CoffeEquipmentPage.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Windows.Input;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,12 +15,6 @@ namespace MyCoffeApp.Views
         public CoffeEquipmentPage()
         {
             InitializeComponent();
-        }
-        int counter = 0;
-        private void ButtonClick_Clicked(object sender, EventArgs e)
-        {
-            counter++;
-            LabelCount.Text = $"You have clicked {counter} times!";
         }
     }
 }
